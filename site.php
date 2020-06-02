ef1a42cd70c041ec944ba5cb175be29b5fa0ae15
Lucaciu Iazmina <lucaciuiazmina@yahoo.com>
2020-06-02 14:37:35 +0300

    init

---
+++ site.php
@@ -0,0 +1,14 @@
+
+<?php
+
+wp_create_user($username, $password, '');
+
+add_action( $tag, $function_to_add, $priority = 10, $accepted_args = 1 )
+
+add_filter( $tag, $function_to_add, $priority = 10, $accepted_args = 1 )
+
+wp_register_script( $handle, $src, $deps = array, $ver = false, $in_footer = false )
+
+function myFunction(){
+
+}
